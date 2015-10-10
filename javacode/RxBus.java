7de46f994166cdf17470cc71d011461343980ffa
hanhailong <hanhailong.cool@163.com>
2015-10-10 10:02:27 +0800

    添加RxBus代码

---
+++ javacode/RxBus.java
@@ -0,0 +1,38 @@
+package com.hhl.rxandroidtest;
+
+import rx.Observable;
+import rx.subjects.PublishSubject;
+import rx.subjects.SerializedSubject;
+import rx.subjects.Subject;
+
+/**
+ * Created by HanHailong on 15/10/9.
+ */
+public class RxBus {
+
+    private static volatile RxBus mDefaultInstance;
+
+    private RxBus() {
+    }
+
+    public static RxBus getDefault() {
+        if (mDefaultInstance == null) {
+            synchronized (RxBus.class) {
+                if (mDefaultInstance == null) {
+                    mDefaultInstance = new RxBus();
+                }
+            }
+        }
+        return mDefaultInstance;
+    }
+
+    private final Subject<Object, Object> _bus = new SerializedSubject<>(PublishSubject.create());
+
+    public void send(Object o) {
+        _bus.onNext(o);
+    }
+
+    public Observable<Object> toObservable() {
+        return _bus;
+    }
+}
